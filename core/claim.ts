a9eefc87ab6d3cb711ce8a9758310ac9c20f3d13
Ivan Schuetz <ivanschuetz@gmail.com>
2022-10-11 13:24:54 +0200

    Convert to Typescript and port first core function

---
+++ core/claim.ts
@@ -0,0 +1,32 @@
+import {
+  Algodv2,
+  makeApplicationCallTxnFromObject,
+  OnApplicationComplete,
+} from "algosdk";
+
+export const claim = async (
+  algod: Algodv2,
+  claimer: string,
+  appId: number,
+  fundsAsset: number
+) => {
+  const params = await algod.getTransactionParams().do();
+
+  const tx = makeApplicationCallTxnFromObject({
+    suggestedParams: params,
+    from: claimer,
+    appIndex: appId,
+    foreignAssets: [fundsAsset],
+    onComplete: OnApplicationComplete.NoOpOC,
+  });
+
+  tx.fee = tx.fee * 2;
+
+  return tx;
+};
+
+export const submit = (algod: Algodv2, tx: Uint8Array | Uint8Array[]) => {
+  algod.sendRawTransaction(tx).do();
+
+  return;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import algosdk from "algosdk";
 import Head from "next/head";
 import { useEffect } from "react";
 import { Foo } from "../components/foo";
+import { claim } from "../core/claim";
 
 const testAlgo = async () => {
   const token =
@@ -10,14 +11,23 @@ const testAlgo = async () => {
   const port = 4001;
   const client = new algosdk.Algodv2(token, server, port);
 
-  console.log("testing algo!!");
+  //   console.log("testing algo!!");
 
-  (async () => {
-    // console.log(await client.status().do());
-    console.log(await client.getTransactionParams().do());
-  })().catch((e) => {
-    console.log(e);
-  });
+  //   (async () => {
+  //     // console.log(await client.status().do());
+  //     console.log(await client.getTransactionParams().do());
+  //   })().catch((e) => {
+  //     console.log(e);
+  //   });
+
+  let tx = await claim(
+    client,
+    "DNQPINWK4K5QZYLCK7DVJFEWRUXPXGW36TEUIHNSNOFYI2RMPG2BZPQ7DE",
+    123,
+    123
+  );
+
+  console.log("tx: %o", tx);
 };
 
 export default function Home() {
