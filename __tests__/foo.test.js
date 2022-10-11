98f51325bf7a1d48ac3af17f854eed83daa608c1
Ivan Schuetz <ivanschuetz@gmail.com>
2022-10-11 09:59:09 +0200

    Add an algosdk test

--- __tests__/foo.test.js
+++ __tests__/foo.test.js
@@ -1,3 +1,14 @@
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
+import algosdk from "algosdk";
+
+test("transaction params", async () => {
+  const token =
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+  const server = "http://127.0.0.1";
+  const port = 4001;
+  const client = new algosdk.Algodv2(token, server, port);
+
+  const params = await client.getTransactionParams().do();
+
+  console.log("params: %o", params);
+  expect(params.firstRound).toBeGreaterThan(0);
 });
