76881dff0693073c1b91cceb74618b5937f942ac
Ivan Schuetz <ivanschuetz@gmail.com>
2022-11-05 13:55:35 +0100

    Migrate css
    Will be modularized later

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,25 @@
 import type { AppProps } from "next/app";
 import { WASMContextProvider } from "../context/WASM";
 
+import "../styles/_global.scss";
+import "../styles/_margins.scss";
+import "../styles/_sidebar.scss";
+import "../styles/_inputs.scss";
+import "../styles/_buttons.scss";
+import "../styles/_modal.scss";
+import "../styles/_my_daos.scss";
+import "../styles/_charts.scss";
+import "../styles/_dao.scss";
+import "../styles/_invest_embedded.scss";
+import "../styles/_funds_activity.scss";
+import "../styles/_withdraw.scss";
+import "../styles/_rightcol.scss";
+import "../styles/_image.scss";
+import "../styles/_progress.scss";
+import "../styles/_toastify.scss";
+import "../styles/_react-calendar.scss";
+import "../styles/_utils.scss";
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <WASMContextProvider>
