1efa02d4bd07624216debaa0235ff934c5d6661c
Ivan Schuetz <ivanschuetz@gmail.com>
2022-11-05 14:30:28 +0100

    Remove "foo" components and signtx example
    (one of the js sdk examples)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import { decodeAddress } from "algosdk";
 import Head from "next/head";
 import { useEffect } from "react";
-import { Foo } from "../components/foo";
 import MyApp from "../components/MyApp";
 import { claim } from "../core/flows/claim";
-import { signTransaction } from "../core/foo";
 import { toAppId, toFundsAsset } from "../core/common/types";
 import { makeAlgod } from "../core/deps";
 import { createAssetsFlow } from "../core/flows/create_assets/createAssetsFlow";
@@ -37,8 +35,6 @@ export default function Home() {
       <main>
         <div>{"Hello Algorand!"}</div>
         <WASMExample />
-        <Foo />
-        <button onClick={() => signTransaction()}>{"Sign a tx"}</button>
         <button onClick={() => createAssetsFlow()}>{"Create assets"}</button>
 
         <MyApp />
