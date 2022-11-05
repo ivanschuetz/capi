ed76b656c73fd266ad52279e30efaaa75a569248
Ivan Schuetz <ivanschuetz@gmail.com>
2022-11-05 14:28:39 +0100

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,25 +4,14 @@ import { useEffect } from "react";
 import { Foo } from "../components/foo";
 import MyApp from "../components/MyApp";
 import { claim } from "../core/flows/claim";
-import MyAlgoConnect from "@randlabs/myalgo-connect";
 import { signTransaction } from "../core/foo";
 import { toAppId, toFundsAsset } from "../core/common/types";
 import { makeAlgod } from "../core/deps";
-import { submit } from "../core/common/tx_utils";
 import { createAssetsFlow } from "../core/flows/create_assets/createAssetsFlow";
 import { WASMExample } from "../components/WASMExample";
 
 const testAlgo = async () => {
   const algod = makeAlgod();
-
-  //   console.log("testing algo!!");
-
-  //   (async () => {
-  //     // console.log(await client.status().do());
-  //     console.log(await client.getTransactionParams().do());
-  //   })().catch((e) => {
-  //     console.log(e);
-  //   });
 
   let tx = await claim(
     algod,
@@ -32,28 +21,6 @@ const testAlgo = async () => {
   );
 
   console.log("tx: %o", tx);
-};
-
-const signTx = async () => {
-  const myAlgoWallet = new MyAlgoConnect();
-  const algod = makeAlgod();
-
-  let tx = await claim(
-    algod,
-    decodeAddress("7ZLNWP5YP5DCCCLHAYYETZQLFH4GTBEKTBFQDHA723I7BBZ2FKCOZCBE4I"),
-    toAppId(123),
-    toFundsAsset(123)
-  );
-
-  const accounts = await myAlgoWallet.connect();
-  const addresses = accounts.map((account) => account.address);
-  console.log("addresses: %o", addresses);
-
-  const signedTxn = await myAlgoWallet.signTransaction(tx.toByte());
-
-  const response = submit(algod, signedTxn.blob);
-
-  console.log("response: %o", response);
 };
 
 export default function Home() {
@@ -71,7 +38,6 @@ export default function Home() {
         <div>{"Hello Algorand!"}</div>
         <WASMExample />
         <Foo />
-        {/* <button onClick={() => signTx()}>{"Sign a tx"}</button> */}
         <button onClick={() => signTransaction()}>{"Sign a tx"}</button>
         <button onClick={() => createAssetsFlow()}>{"Create assets"}</button>
 
