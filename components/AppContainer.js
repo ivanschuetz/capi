05f6b945b664da06c77522274ad6cbece15da630
Ivan Schuetz <ivanschuetz@gmail.com>
2022-11-15 06:16:48 +0100

    Refactor index

---
+++ components/AppContainer.js
@@ -0,0 +1,28 @@
+import Modal from "../modal/Modal";
+import React, { useContext } from "react";
+import { AppContext } from "../context/App";
+import { ToastContainer } from "react-toastify";
+
+export const AppContainer = ({ children }) => {
+  const { deps } = useContext(AppContext);
+
+  return (
+    <div>
+      <div id="container">
+        {children}
+        {deps.modal && (
+          <Modal
+            title={deps.modal.title}
+            onClose={() => ctx.deps.setModal(null)}
+          >
+            {deps.modal.body}
+          </Modal>
+        )}
+        {deps.wcShowOpenWalletModal && (
+          <OpenWalletModal setShowModal={deps.setWcShowOpenWalletModal} />
+        )}
+        <ToastContainer />
+      </div>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,21 @@
-import { Fragment, useContext } from "react";
-import { ToastContainer } from "react-toastify";
+import { useContext } from "react";
 import "react-toastify/dist/ReactToastify.css";
 import { CreateDao } from "../components/CreateDao";
-import Modal from "../modal/Modal";
-import OpenWalletModal from "../wallet/OpenWalletModal";
 import { WireframeWrapper } from "../wireframes/WireframeWrapper";
-
 import { AppContext } from "../context/App";
+import { AppContainer } from "../components/AppContainer";
 
 const Home = () => {
   const ctx = useContext(AppContext);
 
-  const body = () => {
-    return (
-      <Fragment>
-        {/* <div>{connectButton()}</div> */}
-
-        {/* TODO react -> nextjs, */}
-        {/* and remove CreateDao from here */}
-        {/* {navigation()} */}
-        <WireframeWrapper
-          deps={ctx.deps}
-          isGlobal={true}
-          nested={<CreateDao deps={ctx.deps} />}
-        />
-      </Fragment>
-    );
-  };
-
   return (
-    <div>
-      <div id="container">
-        {body()}
-        {ctx.deps.modal && (
-          <Modal
-            title={ctx.deps.modal.title}
-            onClose={() => ctx.deps.setModal(null)}
-          >
-            {ctx.deps.modal.body}
-          </Modal>
-        )}
-        {ctx.deps.wcShowOpenWalletModal && (
-          <OpenWalletModal setShowModal={ctx.deps.setWcShowOpenWalletModal} />
-        )}
-        <ToastContainer />
-      </div>
-    </div>
+    <AppContainer>
+      <WireframeWrapper
+        deps={ctx.deps}
+        isGlobal={true}
+        nested={<CreateDao deps={ctx.deps} />}
+      />
+    </AppContainer>
   );
 };
 
