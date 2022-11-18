f19b8bf0af6dbbb8f2e5561d7466634d2171f137
Ivan Schuetz <ivanschuetz@gmail.com>
2022-11-18 16:02:57 +0100

    Fix PDF view
    Needed some special code for next.js. Src: https://codesandbox.io/s/react-pdf-next-js-y4ev2
    
    It works, but not sure that the implementation is ideal. Review.

--- next.config.js
+++ next.config.js
@@ -7,6 +7,23 @@ const nextConfig = {
   publicRuntimeConfig: {
     version,
   },
+  webpack: (config) => {
+    // load worker files as a urls with `file-loader`
+    config.module.rules.unshift({
+      test: /pdf\.worker\.(min\.)?js/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[contenthash].[ext]",
+            publicPath: "_next/static/worker",
+            outputPath: "static/worker",
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- pdf/PdfView.js
+++ pdf/PdfView.js
@@ -1,28 +1,30 @@
-// import { useState } from "react";
-// import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import { useState } from "react";
 
-// export const PdfView = ({ url }) => {
-//   const [numPages, setNumPages] = useState(null);
-//   const [pageNumber, setPageNumber] = useState(1);
+// import default react-pdf entry
+import { Document, Page, pdfjs } from "react-pdf";
+// import pdf worker as a url, see `next.config.js` and `pdf-worker.js`
+import workerSrc from "../pdf-worker";
 
-//   function onDocumentLoadSuccess({ numPages }) {
-//     setNumPages(numPages);
-//   }
-
-//   return (
-//     <div>
-//       <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
-//         <Page pageNumber={pageNumber} />
-//       </Document>
-//       <p>
-//         Page {pageNumber} of {numPages}
-//       </p>
-//       <button onClick={() => setPageNumber(pageNumber - 1)}>{"prev"}</button>
-//       <button onClick={() => setPageNumber(pageNumber + 1)}>{"next"}</button>
-//     </div>
-//   );
-// };
+pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
 
 export const PdfView = ({ url }) => {
-  return <div>TODO pdf view</div>;
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+      <button onClick={() => setPageNumber(pageNumber - 1)}>{"prev"}</button>
+      <button onClick={() => setPageNumber(pageNumber + 1)}>{"next"}</button>
+    </div>
+  );
 };
