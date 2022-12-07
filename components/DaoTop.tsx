ecdd0ba64deaf9debf66089161d1c71c38bedeb0
Ivan Schuetz <ivanschuetz@gmail.com>
2022-12-07 10:20:29 +0100

    Cleanup

--- components/DaoTop.tsx
+++ components/DaoTop.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react"
 import { DaoJs } from "wasm/wasm"
-import share from "../images/svg/share.svg"
-import twitter from "../images/svg/twitter.svg"
 import Modal from "../modal/Modal"
 import { SetBool } from "../type_alias"
 import SvgShare from "./icons/SvgShare"
