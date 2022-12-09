3580c92106668c20198c20169f961ff956622717
Ivan Schuetz <ivanschuetz@gmail.com>
2022-12-09 11:49:27 +0100

    Migrate funds activity to tailwind (tablet)

--- components/FundsActivityEntry.tsx
+++ components/FundsActivityEntry.tsx
@@ -2,8 +2,6 @@ import ReactTooltip from "react-tooltip"
 import { FundsActivityViewData } from "wasm/wasm"
 import { Deps } from "../context/AppContext"
 import funds from "../images/funds.svg"
-import arrowDown from "../images/svg/arrow-down.svg"
-import arrowUp from "../images/svg/arrow-up.svg"
 import SvgArrowDown from "./icons/SvgArrowDown"
 import SvgArrowUp from "./icons/SvgArrowUp"
 
@@ -26,9 +24,9 @@ const DesktopEntryView = ({ entry }: { entry: FundsActivityViewData }) => {
     <div className="mb-8 flex justify-between border border-solid border-ne3 p-6">
       {/* center arrow internally so justify-between works in right column (date at top edge and link at bottom edge) */}
       <div className="center-children mr-4">
-        <ArrowUpOrDown entry={entry} sizeClasses="w-14 h-14" />
+        <Arrow entry={entry} addClass="w-14 h-14" />
       </div>
-      <AmountView entry={entry} />
+      <AmountView entry={entry} widthClass={"w-48"} />
       <div className="mr-2 grow basis-0">
         <AddressRow entry={entry} />
         <div className="shrink-3 mb-5 w-full text-45 text-ne4 sm:mb-0 sm:w-auto">
@@ -49,24 +47,18 @@ const AddressRow = ({ entry }: { entry: FundsActivityViewData }) => {
     </div>
   )
 }
+
 const TabletEntryView = ({ entry }) => {
   return (
-    <div className="funds_act_entry-tab">
-      <img
-        className="arrow funds-arrow-icon d-mobile-none"
-        src={entry.is_income === "true" ? arrowUp.src : arrowDown.src}
-        alt="arrow"
-      />
-      <div className="funds_act_entry__body">
-        <div className="d-flex align-center order-2">
-          <div className="wallet">{entry.address}</div>
-          <div className="ellipse tablet-none"></div>
+    <div className="mb-6 flex gap-4 border border-solid border-ne3 p-5">
+      <Arrow entry={entry} addClass="w-12 h-12" />
+      <div className="flex flex-grow flex-col">
+        <div className="text-50 font-bold text-te">{entry.address}</div>
+        <div className="flex flex-row items-center gap-4">
+          <AmountView entry={entry} widthClass={"w-24"} />
+          <div className="">{fundsActivityEntryLabel(entry)}</div>
         </div>
-        <div className="d-flex align-center order-1">
-          <AmountView entry={entry} />
-          <div className="status">{fundsActivityEntryLabel(entry)}</div>
-        </div>
-        <div className="description order-3">{entry.description}</div>
+        <div className="mr-5 text-40 text-te">{entry.description}</div>
       </div>
       <DetailsLink entry={entry} />
     </div>
@@ -75,7 +67,7 @@ const TabletEntryView = ({ entry }) => {
 
 const DetailsLink = ({ entry }: { entry: FundsActivityViewData }) => {
   return (
-    <div className="flex flex-col justify-between text-right">
+    <div className={`flex flex-shrink-0 flex-col justify-between text-right`}>
       <div className="text-te2">{entry.date}</div>
       <a
         href={entry.tx_link}
@@ -97,9 +89,15 @@ export const fundsActivityEntryLabel = (entry: FundsActivityViewData) => {
   }
 }
 
-const AmountView = ({ entry }: { entry: FundsActivityViewData }) => {
+const AmountView = ({
+  entry,
+  widthClass,
+}: {
+  entry: FundsActivityViewData
+  widthClass: string
+}) => {
   return (
-    <div className="flex w-48 items-center">
+    <div className={`flex ${widthClass} items-center`}>
       <img className="mr-2 h-6 w-6" src={funds.src} alt="funds" />
       <div className={"text-60 font-bold text-te"}>{amountTextView(entry)}</div>
     </div>
@@ -122,16 +120,16 @@ export const amountTextView = (entry: FundsActivityViewData) => {
   }
 }
 
-const ArrowUpOrDown = ({
+const Arrow = ({
   entry,
-  sizeClasses,
+  addClass,
 }: {
   entry: FundsActivityViewData
-  sizeClasses: string
+  addClass: string
 }) => {
   if (entry.is_income === "true") {
-    return <SvgArrowUp className={`fill-sec ${sizeClasses}`} />
+    return <SvgArrowUp className={`fill-sec ${addClass} flex-shrink-0`} />
   } else {
-    return <SvgArrowDown className={`fill-pr ${sizeClasses}`} />
+    return <SvgArrowDown className={`fill-pr ${addClass} flex-shrink-0`} />
   }
 }
